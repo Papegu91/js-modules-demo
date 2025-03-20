806cd90c35173d55833f7a09adcc06cf341703ed
Papegu91 <petergudoi1@gmail.com>
2025-03-20 23:29:32 +0300

    Added JavaScript modules example

---
+++ main.js
@@ -0,0 +1,5 @@
+// main.js - Importing the module
+import { add, multiply } from './math.js';
+
+console.log(add(5, 3)); // 8
+console.log(multiply(4, 2)); // 8
---
+++ math.js
@@ -0,0 +1,8 @@
+// math.js - This is a module
+export function add(a, b) {
+    return a + b;
+}
+
+export function multiply(a, b) {
+    return a * b;
+}
